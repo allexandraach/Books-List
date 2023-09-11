1b719ae9d8a3ff6c532f82a19ef262ba2ba8ee3b
Achimov Alexandra <achimov_alexandra@yahoo.com>
2023-09-11 19:51:41 +0300

    implemented clear filter button

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,8 +4,8 @@ export function SearchBar({ searchBooks, search, filterBooks }) {
 
   return <form id="searchBarForm">
 
-    <input type="text" value={searchBooks} placeholder="Search books" 
-    onChange = {(e) =>  { search(e.target.value)}} />
+    <input type="text" value={searchBooks} placeholder="Search books"
+      onChange={(e) => { search(e.target.value) }} />
 
     <br />
 
@@ -14,10 +14,10 @@ export function SearchBar({ searchBooks, search, filterBooks }) {
       <div>
         Filter by:
         <label htmlFor="filterBooks">
-          <select name="filter" value="default" onChange={(e) => {filterBooks(e.target.name, e.target.value )}} >
+          <select name="filter" value="default" onChange={(e) => { filterBooks(e.target.name, e.target.value) }} >
             <option value="default" disabled hidden> </option>
-              <option value="currentlyReading"> currently reading</option>
-              <option value="favourite"> favourite</option>
+            <option value="currentlyReading"> currently reading</option>
+            <option value="favourite"> favourite</option>
           </select>
         </label>
       </div>
@@ -25,15 +25,20 @@ export function SearchBar({ searchBooks, search, filterBooks }) {
       <div>
         Sort by:
         <label htmlFor="sortBooks">
-          <select name="sort" value="default" onChange={(e) => {filterBooks(e.target.name, e.target.value )}} >
+          <select name="sort" value="default" onChange={(e) => { filterBooks(e.target.name, e.target.value) }} >
             <option value="default" disabled hidden> </option>
-              <option value="author"> author</option>
-              <option value="genre"> genre</option>
+            <option value="author"> author</option>
+            <option value="genre"> genre</option>
           </select>
         </label>
       </div>
 
+      <div>
+        <a id="clearBtn" href="/"> Clear </a>
+      </div>
+
     </div>
+
 
   </form>
 };
